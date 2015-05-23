e609bbc887b1d5aea4a7e9d5ed377aeecc387739
Zack Moss <zmyth97@gmail.com>
2015-05-23 12:19:43 -0600

    Added Some "Fill in" Graphics for Sky, Ground, and Castles to start off the GameScreen

--- core/src/com/desitum/castleWars/world/GameRenderer.java
+++ core/src/com/desitum/castleWars/world/GameRenderer.java
@@ -2,9 +2,12 @@ package com.desitum.castleWars.world;
 
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.desitum.castleWars.data.Assets;
 import com.desitum.castleWars.objects.Card;
 import com.desitum.castleWars.screens.GameScreen;
 import com.desitum.castleWars.screens.MenuScreen;
+
+import java.awt.Menu;
 
 /**
  * Created by Zmyth97 on 2/25/2015.
@@ -28,14 +31,20 @@ public class GameRenderer {
         gameCam.update();
         gameBatch.setProjectionMatrix(gameCam.combined);
 
-       //Draw stuff
+       drawWorld();
     }
 
+    private void drawWorld(){
+        gameBatch.draw(Assets.gameSky, 0, 0, GameScreen.SCREEN_WIDTH, GameScreen.SCREEN_HEIGHT);
+        gameBatch.draw(Assets.playerCastle, ((GameScreen.SCREEN_WIDTH/6)), 0,(GameScreen.SCREEN_WIDTH/4), (GameScreen.SCREEN_HEIGHT/2));
+        gameBatch.draw(Assets.computerCastle, ((GameScreen.SCREEN_WIDTH/1.75f)), 0, (GameScreen.SCREEN_WIDTH/4), (GameScreen.SCREEN_HEIGHT/2));
+        gameBatch.draw(Assets.gameGround, 0, 0, GameScreen.SCREEN_WIDTH, (GameScreen.SCREEN_HEIGHT/4));
+    }
     public OrthographicCamera getCam() {
         return gameCam;
     }
 
     public void resetCam() {
-        gameCam.position.set(MenuScreen.SCREEN_WIDTH / 2, MenuScreen.SCREEN_HEIGHT / 2, 0);
+        gameCam.position.set(GameScreen.SCREEN_WIDTH / 2, GameScreen.SCREEN_HEIGHT / 2, 0);
     }
 }
